7111f7daa36e8a9c40c94d4430f72e1a3db56fdc
Pavel Rabekin <pavelrabekin@gmail.com>
2022-02-23 11:11:34 +0300

    23.02.2022 - Description

--- Python/4pyu/Description.py
+++ Python/4pyu/Description.py
@@ -27,12 +27,16 @@ Additional notes:
 There are performance tests consisted of very big numbers and arrays of size at least 30000. Please write an efficient algorithm to prevent timeout.
 """
 
-def solution(a):
-    # while set(a)==1:
-    a=a.index(max(a))
-
-    return a
+def solution(array):
+    the_same=len(set(array))
+    while the_same!=1:
+        array.sort()
+        max_from_the_array=max(array)
+        index=array.index(max_from_the_array)        
+        array[index]=array[index]-array[index-1]
+        the_same=len(set(array))
+    return sum(array)
 
 #--------------
 
-print(solution([6, 9, 21]))
+print(solution([1190943, 438567, 15640512, 3240663, 4059072, 2090175, 529308, 5972028, 16386972, 10116447, 489375, 1170672, 19136607, 302847, 5053047, 9880503, 11088063]))
