748d2da1174352fedc8dc4c3a1ec0be5b196c289
Pavel Rabekin <pavelrabekin@gmail.com>
2022-03-17 16:25:17 +0300

    update

---
+++ Python/6pyu/Doors in the school.py
@@ -0,0 +1,7 @@
+"""
+hese strange children pass by some doors they change their status (i.e. Open -> Closed, Closed -> Open). Each student has their number, and each i-th student alters the status of every i-th door. For example: when the first child comes to the schools, he changes every first door (he opens all of them). The second one changes the status of every second door (he closes some doors: the 2nd, the 4th and so on). Finally, when the last one – the n-th – comes to the school, he changes the status of each n-th door (there's only one such door, though).
+
+You need to count how many doors are left opened after all the students have come.
+"""
+def doors(n):   
+    return int(n**0.5)
