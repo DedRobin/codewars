236cc9dd894b790c601fe8f07fa6dc0b7704195c
Pavel Rabekin <pavelrabekin@gmail.com>
2022-04-27 16:21:17 +0300

    22.04.2022

---
+++ Python/6pyu/Counting Duplicates.py
@@ -0,0 +1,7 @@
+'https://www.codewars.com/kata/54bf1c2cd5b56cc47f0007a1'
+
+from collections import Counter
+
+def duplicate_count(text):
+    duplicates=Counter(text.lower()).items()
+    return sum([1 for x in duplicates if x[1]>1])    
