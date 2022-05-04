b1b79736321d1bb717ae2340327d4512d15cd8ad
Pavel Rabekin <pavelrabekin@gmail.com>
2022-05-04 15:13:41 +0300

    04.05.2022

--- Python/6pyu/Smallest Permutation.py
+++ Python/6pyu/Smallest Permutation.py
@@ -1,11 +1,8 @@
 'https://www.codewars.com/kata/5fefee21b64cc2000dbfa875'
 
-import string
-
-
 def min_permutation(n: int):
     if n==0: return 0
-    if n<0: minus=True
+    minus=True if n<0 else False
     lst=list(str(abs(n)))
     lst.sort()
     count_of_zero=lst.count('0')    
@@ -14,5 +11,3 @@ def min_permutation(n: int):
         lst.insert(count_of_zero,zero)
     n=int(''.join(lst))
     return -n if minus else n
-
-print(min_permutation(-20))
