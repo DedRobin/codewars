e8141df9168fa1b82c8f7bba8cbe11e2d980f2e8
Pavel Rabekin <pavelrabekin@gmail.com>
2022-05-06 11:56:45 +0300

    06.05.2022

--- Python/7pyu/Bingo ( Or Not ).py
+++ Python/7pyu/Bingo ( Or Not ).py
@@ -1,6 +1,6 @@
 'https://www.codewars.com/kata/5a1ee4dfffe75f0fcb000145'
 
 def bingo(array):
-    return 'WIN' if set([chr(number+96) for number in array])&set(['b','i','n','g','o'])==set(['b','i','n','g','o']) else 'LOSE'
+    return 'WIN' if set(array)&set([2, 9, 14, 7, 15])==set([2, 9, 14, 7, 15]) else 'LOSE'
 
-print(bingo([1, 2, 3, 7, 5, 14, 7, 15, 10]))
+print(bingo([1, 2, 3, 7, 5, 14, 7, 15, 9, 10]))
