a49a063d1a1bed15b3128012a82205f25bb1e2ef
Pavel Rabekin <pavelrabekin@gmail.com>
2022-05-11 11:05:48 +0300

    11.05.2022

--- Python/6pyu/Where is my parent!(cry).py
+++ Python/6pyu/Where is my parent!(cry).py
@@ -1,8 +1,4 @@
-'https://www.codewars.com/kata/58539230879867a8cd00011c'
-def find_children(dancing_brigade):
-    dancing_brigade=sorted(dancing_brigade)
-    all_letters=sorted(map(lambda x: x.lower(),dancing_brigade))
-    upper_letters=[x for x in dancing_brigade if not x.islower()]
-    for upper_letter in upper_letters:
-        all_letters[all_letters.index(upper_letter.lower())]=upper_letter
-    return ''.join(all_letters)
+#'https://www.codewars.com/kata/58539230879867a8cd00011c'
+
+def find_children(s):
+    return ''.join(sorted(sorted(s), key=str.lower))
