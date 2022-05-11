4b202a2d8af968cde063184c14f8f711a2a751d1
Pavel Rabekin <pavelrabekin@gmail.com>
2022-05-11 14:27:59 +0300

    11.05.2022

--- Python/5pyu/Longest sequence with zero sum.py
+++ Python/5pyu/Longest sequence with zero sum.py
@@ -8,6 +8,3 @@ def max_zero_sequence(arr):
                 if len(longest_zero_sum)<len(arr[start:end]):
                     longest_zero_sum=arr[start:end]
     return longest_zero_sum
-    # return sum(arr)
-
-print(max_zero_sequence([1, 2, -3, 7, 8, -16]))
