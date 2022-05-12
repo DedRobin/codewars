b8c5961f752d5f675fe260a04b68a6e481789485
Pavel Rabekin <pavelrabekin@gmail.com>
2022-05-12 10:38:09 +0300

    12.05.2022

--- Python/5pyu/RGB To Hex Conversion.py
+++ Python/5pyu/RGB To Hex Conversion.py
@@ -3,4 +3,6 @@
 def rgb(r, g, b):
     r,g,b=(0 if num<0 else num for num in (r,g,b))
     r,g,b=(255 if num>255 else num for num in (r,g,b))
-    return ''.join(['{:02X}'.format(num)for num in (r,g,b)])
+    return ('{:02X}'*3).format(r,g,b)
+
+print(rgb(-1,0,257))
