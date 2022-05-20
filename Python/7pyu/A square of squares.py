bb6a600dd76f0d5f248173235e4c24e75ca9fca7
Pavel Rabekin <pavelrabekin@gmail.com>
2022-05-21 00:55:17 +0300

    A square of squares.py was changed. Twice :).

--- Python/7pyu/A square of squares.py
+++ Python/7pyu/A square of squares.py
@@ -1,5 +1,4 @@
 # https://www.codewars.com/kata/54c27a33fb7da0db0100040e
 
-
 def is_square(n: int):
-    return True if n >= 0 and (n**0.5 * 10) % 10 == 0 else False
+    return True if n >= 0 and (n ** 0.5 * 10) % 10 == 0 else False
