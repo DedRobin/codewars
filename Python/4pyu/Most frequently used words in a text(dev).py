5aa3145eb4fdf04ca4cbde7a29b98aebcceaad11
Pavel Rabekin <pavelrabekin@gmail.com>
2022-05-23 21:17:57 +0300

    Most frequently used words in a text.py

--- Python/4pyu/Most frequently used words in a text(dev).py
+++ Python/4pyu/Most frequently used words in a text(dev).py
@@ -1,47 +1,42 @@
 # https://www.codewars.com/kata/51e056fe544cf36c410000fb
+
+from collections import Counter
+
+
 def top_3_words(text):
-    text = text.lower().strip()
+    # I convert the string to lowercase.
+    text = text.lower()
 
+    # Using list comprehensions, we filter the string from characters we don’t need.
     valid_characters = " 'abcdefghijklmnopqrstuvwxyz"
+    filter_text = [x if x in valid_characters else " " for x in text]
 
-    invalid_characters = {x for x in text if x not in valid_characters}
+    # Then we combine the resulting list into a string and we split it.
+    # We remove whitespace characters.
+    filter_text = "".join(filter_text)
+    filter_text = filter_text.split()
 
-    aaa = "".join([x for x in text if x.isalpha()])
-    if not aaa:
-        return []
-    text = text.split()
+    # Create function which return True if string contain alphabet characters.
+    def contain_alpha(string):
+        for symbol in string:
+            if symbol in "abcdefghijklmnopqrstuvwxyz":
+                return True
+        return False
 
-    text = list(map(lambda z: z.strip(), text))
+    # Filter out our string and except characters which contain only apostrophe.
+    filter_text = [x for x in filter_text if contain_alpha(x)]  # for single apostrophes
 
-    for x in invalid_characters:
-        text = list(map(lambda z: z.replace(x, ""), text))
+    # Finally, we use Counter collection to find 3 top words in input text.
+    most_common = Counter(filter_text).most_common(3)
 
-    text = [x for x in text if (x.isalpha() and "'" not in x) or (not x.isalpha() and "'" in x)]
+    return [x[0] for x in most_common[0:3]]
 
-    most_common = [(text.count(x), x) for x in set(text)]
-
-    most_common = sorted(most_common, key=lambda x: x[0], reverse=True)
-
-    return [x[1] for x in most_common[0:3]]
-
-
-print(top_3_words("  //wont won't won't "))
-# print(top_3_words("e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e"))
-print(top_3_words("  '  "))
-# print(top_3_words("""In a village of La Mancha, the name of which I have no desire to call to
-# mind, there lived not long since one of those gentlemen that keep a lance
-# in the lance-rack, an old buckler, a lean hack, and a greyhound for
-# coursing. An olla of rather more beef than mutton, a salad on most
-# nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra
-# on Sundays, made away with three-quarters of his income."""))
-# print(top_3_words("  , e   .. "))
-
-if __name__ == "__main__":
-    string = "FYw .'-won't:nFnXDzCcFx;ksB;FYw_FYw/,!/FYw!ksB;,nFnXDzCcFx:_ksB!FYw;!,?kyIo:;:_ksB;,-kyIo? _ ?nFnXDzCcFx_ksB/kyIo -?/;nFnXDzCcFx.:.-FYw_nFnXDzCcFx /nFnXDzCcFx?/ __ksB_- nFnXDzCcFx/?.FYw:,.!:FYw_-_:-nFnXDzCcFx/kyIo/kyIo.ksB_:nFnXDzCcFx/-:nFnXDzCcFx-FYw!.:ksB-nFnXDzCcFx!/.;/kyIo/ ,,FYw  -.;ksB;-ksB/,ksB?! ,kyIo!!; ,ksB_!?nFnXDzCcFx:; ,:ksB:;- ksB.ksB_:ksB ?.,kyIo: ,nFnXDzCcFx!;/!FYw-?!kyIo-- :kyIo_,;ksB_kyIo!??. ksB:FYw _/ksB/:,kyIo.?_;,nFnXDzCcFx,,/ksB_;.:FYw-_ ::kyIo,ksB;,:-nFnXDzCcFx!:-kyIo-?!nFnXDzCcFx?-_ksB.FYw/nFnXDzCcFx!  ?;ksB,FYw,FYw/ /._ksB?nFnXDzCcFx:ksB_ !;/FYw:/ nFnXDzCcFx ,nFnXDzCcFx: ::kyIo/,,nFnXDzCcFx: ksB FYw-;FYw,ksB -/_,kyIo -:FYw!_;?ksB/_?;.nFnXDzCcFx_"
-    # string = "  '  "
-    print(string)
-    a = [x if x in " 'abcdefghijklmnopqrstuvwxyz" else " " for x in string.lower()]
-    print("".join(a))
-    print("".join(a).split())
-    a = [x for x in a if x != "'"]
-    print("".join(a).split())
+# ---> Using import "collection.Counter" and "re"
+#
+# from collections import Counter
+# import re
+#
+#
+# def top_3_words(text):
+#     c = Counter(re.findall(r"[a-z']+", re.sub(r" '+ ", " ", text.lower())))
+#     return [w for w,_ in c.most_common(3)]
