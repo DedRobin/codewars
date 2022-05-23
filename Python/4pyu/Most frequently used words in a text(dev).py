8a2cfa2406c6559c3d093d7248f7c159dfca5290
Pavel Rabekin <pavelrabekin@gmail.com>
2022-05-23 15:23:31 +0300

    v. 0.1

--- Python/4pyu/Most frequently used words in a text(dev).py
+++ Python/4pyu/Most frequently used words in a text(dev).py
@@ -1,28 +1,31 @@
 # https://www.codewars.com/kata/51e056fe544cf36c410000fb
 def top_3_words(text):
     text = text.lower().strip()
-    a = [32, 39] + list(range(97, 123))
-    sym = {x for x in text if ord(x) not in a}
-    contain_alpha = False
-    contain_spec_symb = False
-    for x in text:
-        if ord(x) in a:
-            check = True
-            break
-    if not check:
+
+    valid_characters = " 'abcdefghijklmnopqrstuvwxyz"
+
+    invalid_characters = {x for x in text if x not in valid_characters}
+
+    aaa = "".join([x for x in text if x.isalpha()])
+    if not aaa:
         return []
-    for z in sym:
-        text = text.replace(z, "")
-    # list_with_words = (text.replace(x, "") for x in sym)
-    list_with_words = text.split()
-    set_with_words = set(text.split())
-    result = [(text.count(word), word) for word in set_with_words]
-    result = sorted(result, reverse=True)
-    result = [x[1] for x in result][0:3]
-    return result
+    text = text.split()
+
+    text = list(map(lambda z: z.strip(), text))
+
+    for x in invalid_characters:
+        text = list(map(lambda z: z.replace(x, ""), text))
+
+    text = [x for x in text if (x.isalpha() and "'" not in x) or (not x.isalpha() and "'" in x)]
+
+    most_common = [(text.count(x), x) for x in set(text)]
+
+    most_common = sorted(most_common, key=lambda x: x[0], reverse=True)
+
+    return [x[1] for x in most_common[0:3]]
 
 
-# print(top_3_words("  //wont won't won't "))
+print(top_3_words("  //wont won't won't "))
 # print(top_3_words("e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e"))
 print(top_3_words("  '  "))
 # print(top_3_words("""In a village of La Mancha, the name of which I have no desire to call to
@@ -31,3 +34,4 @@ print(top_3_words("  '  "))
 # coursing. An olla of rather more beef than mutton, a salad on most
 # nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra
 # on Sundays, made away with three-quarters of his income."""))
+# print(top_3_words("  , e   .. "))
